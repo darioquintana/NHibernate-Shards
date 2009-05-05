a1eea1d38cd09e069485faff7164d6e40a1394be
Dario Quintana <contacto@darioquintana.com.ar>
2009-05-05 02:58:35 +0000

    Inherits from Fixture with mocks. Need tests to be passed.

--- src/NHibernate.Shards.Test/Criteria/AddOrderEventTest.cs
+++ src/NHibernate.Shards.Test/Criteria/AddOrderEventTest.cs
@@ -1,41 +1,30 @@
 ﻿using NHibernate.Shards.Criteria;
+using NHibernate.Shards.Test.Mock;
 using NUnit.Framework;
-using Rhino.Mocks;
 
 namespace NHibernate.Shards.Test.Criteria
 {
+	[TestFixture]
+	public class AddOrderEventTest : TestFixtureBaseWithMock
+	{
+		[Test]
+		public void TestOnOpenSession()
+		{
+			var @event = new AddOrderEvent(null);
+			bool[] called = {false};
+			var crit = Mock<ICriteria>();
 
-    [TestFixture]
-    public class AddOrderEventTest
-    {
-        protected MockRepository mocks;
-        protected MockRepository Mocks
-        {
-            get { return mocks; }
-        }
-        protected T Mock<T>()
-        {
-            return Mocks.StrictMock<T>();
-        }
+			using (Mocks.Record())
+			{
+				//Expect.Call(crit).Do(called[0]=true).Return(null);
+			}
 
-        [Test]
-        public void TestOnOpenSession()
-        {
-            var @event = new AddOrderEvent(null);
-            bool[] called = {false};
-            var crit = Mock<ICriteria>();
-            
-            using (Mocks.Record())
-            {
-                //Expect.Call(crit).Do(called[0]=true).Return(null);
-            }
-
-            using (Mocks.Playback())
-            {
-                @event.OnEvent(crit);
-                Assert.Equals(called[0], true);
-            }
-            /*
+			using (Mocks.Playback())
+			{
+				@event.OnEvent(crit);
+				Assert.Equals(called[0], true);
+			}
+			/*
                 AddOrderEvent event = new AddOrderEvent(null);
                 final boolean[] called = {false};
                 Criteria crit = new CriteriaDefaultMock() {
@@ -48,6 +37,6 @@ namespace NHibernate.Shards.Test.Criteria
                 event.onEvent(crit);
                 assertTrue(called[0]);
                          */
-        }
-    }
+		}
+	}
 }
